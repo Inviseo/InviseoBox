9dc72368d128e6a28050c34cc5652a8830778b7a
Alexandre Maurice <alexandre@inviseo.fr>
2024-03-01 10:06:38 +0100

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from pymodbus.client import AsyncModbusSerialClient
 
-
+####
 # Chemin du port série
 serial_port = "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
 
